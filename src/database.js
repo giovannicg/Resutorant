5e6e2b7ddd1f35b83f491dae71c6b05b6d53ad95
Giovanni Capote Garcia <giovannicapotegarcia@gmail.com>
2022-04-12 12:34:24 -0500

    cambios database

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const MONGODB_URI =process.env.MONGODB_URI
+const MONGODB_URI ='mongodb+srv://admin:admin@cluster0.hy43r.mongodb.net/RestaurantDB?retryWrites=true&w=majority'
 mongoose.connect(MONGODB_URI)
 .then(db =>console.log('database is connected'))
 .catch(err => console.log(err));
